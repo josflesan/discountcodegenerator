518c4f6265c25c9c3cfcac5fa55af4b1dd031744
Alejandro Santiago <44524995+Alejandro202@users.noreply.github.com>
2018-11-25 13:49:00 +0000

    Main Program
    Creation of the main program, only includes the generate code function.

---
+++ main.py
@@ -0,0 +1,34 @@
+# DiscountCodeGenerator  Copyright (C) 2019  Lemur
+# This program comes with ABSOLUTELY NO WARRANTY.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions.
+
+CODE_DELIMITER = "-"
+FILE_DELIMITER = "/"
+
+
+def generate_code():
+    code = ""
+
+    discount_value = int(input("Input discount value: "))
+    if (discount_value > 0) and (discount_value < 100):
+        code += str(discount_value) + CODE_DELIMITER
+    else:
+        return 13  # Error Code 13: The data is invalid.
+
+    code_id = f_codeID(seed)
+    code += str(code_id) + CODE_DELIMITER
+
+    today_date += f_today()
+    code += str(today_date) + CODE_DELIMITER
+
+    expiration_days = int(input("Input expiration days: "))
+    if expiration_days > 0:
+        code += str(expiration_days) + CODE_DELIMITER
+    else:
+        return 13  # Error Code 13: The data is invalid.
+
+    valid_flag = f_validate(code)
+    code += valid_flag
+
+    return code
