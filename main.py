b4c0ec90c165ada9bb057e4e495c4a4412496979
Alestiago <alejandro.sadi3@gmail.com>
2018-12-26 18:00:59 +0000

    Created a nice looking header.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+###################################################################################
+#                       H   E   A   D   E   R
+#
 # DiscountCodeGenerator  Copyright (C) 2019  Lemur
 # This program comes with ABSOLUTELY NO WARRANTY.
 # This is free software, and you are welcome to redistribute it
 # under certain conditions.
-
+#
+###################################################################################
 CODE_DELIMITER = "-"
 FILE_DELIMITER = "/"
 
