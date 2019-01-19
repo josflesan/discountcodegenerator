a64c573b54ece7416216b626200104e6a81866a8
Alestiago <alejandro.sadi3@gmail.com>
2019-01-19 19:50:48 +0000

    Added tkinter GUI to select directory

--- main.py
+++ main.py
@@ -357,3 +357,9 @@ def main():
 
 
 main()
+
+
+# from tkinter.filedialog import *
+# Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+# filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+# print(filename)
