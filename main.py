3c2eca8bd5f19877d35254c4866b39b420127563
Joflesan <34693834+josflesan@users.noreply.github.com>
2019-01-19 00:46:10 +0000

    Updated main.py
    File Menu sorted and Main Menu prepared.
    Main Menu functions to go.
    
    19/01/2019

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import sys
+from dependencies.txtFileManager.txtFileManager.txtFileManager import *
+
 ###################################################################################
 #                       H   E   A   D   E   R
 #
@@ -12,33 +16,35 @@
 CODE_DELIMITER = "-"
 FILE_DELIMITER = "/"
 
+# def generate_code():
+#     code = ""
+#
+#     discount_value = int(input("Input discount value: "))
+#     if (discount_value > 0) and (discount_value < 100):
+#         code += str(discount_value) + CODE_DELIMITER
+#     else:
+#         return 13  # Error Code 13: The data is invalid.
+#
+#     code_id = f_codeID(seed)
+#     code += str(code_id) + CODE_DELIMITER
+#
+#     today_date += f_today()
+#     code += today_date + CODE_DELIMITER
+#
+#     expiration_days = int(input("Input expiration days: "))
+#     if expiration_days > 0:
+#         code += str(expiration_days) + CODE_DELIMITER
+#     else:
+#         return 13  # Error Code 13: The data is invalid.
+#
+#     valid_flag = f_validate(code)
+#     code += valid_flag
+#
+#     return code
 
-def generate_code():
-    code = ""
 
-    discount_value = int(input("Input discount value: "))
-    if (discount_value > 0) and (discount_value < 100):
-        code += str(discount_value) + CODE_DELIMITER
-    else:
-        return 13  # Error Code 13: The data is invalid.
-
-    code_id = f_codeID(seed)
-    code += str(code_id) + CODE_DELIMITER
-
-    today_date += f_today()
-    code += today_date + CODE_DELIMITER
-
-    expiration_days = int(input("Input expiration days: "))
-    if expiration_days > 0:
-        code += str(expiration_days) + CODE_DELIMITER
-    else:
-        return 13  # Error Code 13: The data is invalid.
-
-    valid_flag = f_validate(code)
-    code += valid_flag
-
-    return code
-
+###################################################################################
+#                      M  I  S  C     F  U  N  C  T  I  O  N  S
 
 def f_today():
     import datetime
@@ -48,19 +54,168 @@ def f_today():
     return today
 
 
-def create_file():
+def create_dir(name, path):
+
+    files = ['config', 'stats', 'activecodes', 'inactivecodes', 'README']
+
+    while True:
+        try:
+            os.chdir(path)  # Change directory to path entered by the user
+            break
+
+        except OSError:
+            print("Invalid path entered...")
+            path = input("\nPlease, try again: ")
+            continue
+
+    os.mkdir(name)  # Create the new directory
+
+    os.chdir(os.path.join(path, name))  # Change into newly created directory
+
+    for f in files:
+        with open(f+'.txt') as fh: # Create different files in the directory
+            if f == "README":
+                fh.write("We recommend not to delete or modify these files under any \n"
+                         "circumstances for it may lead to irregularities in the module's \n"
+                         "functioning. \n"
+                         "\n"
+                         "~ LEMURER COMPANY")
+
+            fh.close()
+
+
+def open_dir(path):
+
+    files = ['config', 'stats', 'activecodes', 'inactivecodes', 'README']
+
+    if not os.path.exists(path):
+        while True:
+            decision = input("\nPath doesn't exist, would you like to create it?: (Yes, or No) ").upper()[0]
+
+            if decision in ['Y', 'N']:
+                break
+            else:
+                print("Please, only yes or no here... ")
+                continue
+
+        if decision == "Y":
+            name = path.split("/")[-1]  # Get folder name
+            path = "/".join(path.split("/")[:-1])  # Get path name
+            create_dir(name, path)
+
+        else:
+            sys.exit()  # Other action?
+
+    os.chdir(path)
+    validfile = []
+    for file in os.listdir(path):
+        f = os.path.splitext(file)[1]
+        validfile.append(f)
+
+    valid = True
+
+    for i in validfile:
+        if i not in files:
+            print("Invalid path was entered, correct tree structure should be present in the file...")
+            valid = False
+
+    if valid:
+        print("Path opened successfully")
+        return 1
+    else:
+        return 0
+
+# ---- MAIN FUNCTIONS ----
+
+
+def generate_code():
     pass
 
+
+def active_codes():
+    pass
+
+
+def expired_codes():
+    pass
+
+
+def settings():
+    pass
+
+
+def file_stats():
+    pass
+
+
+def credit():
+    pass
+
+
 ###################################################################################
-#                               M   A   I   N
-def main():
+
+
+###################################################################################
+#                  M   A   I   N          M    E    N    U
+
+def main_menu():
+    menu = {1: ("Generate Code", generate_code),
+            2: ("Active Codes", active_codes),
+            3: ("Expired/ Validated Codes", expired_codes),
+            4: ("Settings", settings),
+            5: ("File Stats", file_stats),
+            6: ("Credit", credit)}
+
+    for key in sorted(menu.keys()):
+        print(str(key) + ": " + menu[key][0])
+
+    while True:
+        try:
+            choice = int(input("\nPlease input your choice (1-6): "))
+
+            if 1 <= choice <= 6:
+                menu.get(choice)[1]()  # Call the option chosen
+                break
+
+            else:
+                print("Only 6 options, try again...")
+                continue
+
+        except ValueError:
+            print("\nOnly integers here, please...")
+            continue
+
+###################################################################################
+
+
+###################################################################################
+#                   F   I   L   E       M   E   N   U
+
+
+def file_menu():
     # This option will be an action when button is pressed
-    option = int(input("Open File (1) or Create File (0): "))
-    if option == 1:
-        # Call Open File
-    if option == 2:
-        # Call Create FIle
+    while True:
+        try:
+            option = int(input("Open File (1) or Create File (0): "))
+            break
 
+        except ValueError:
+            print("\nOption input should be an integer... ")
+            continue
+
+    if option == 1:
+        path = input("\nPlease, input the path where your folder exists: ")
+        valid = open_dir(path)
+        if not valid:
+            main()  # if invalid, rerun main menu
+        pass
+    elif option == 2:
+        name = input("\nPlease, input the folder name: ")
+        path = input("\nPlease enter the path where you want to create it in the form 'folder/subfolder': ")
+        create_dir(name, path)
+        pass
 
 ###################################################################################
-main()
+
+if __name__ == "__main__":
+    file_menu()
